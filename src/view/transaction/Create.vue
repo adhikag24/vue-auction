03133c67d97c7e5851be952ba9434a77683ec7ac
Adhika Guna <adhikag167@gmail.com>
2021-10-21 23:26:44 +0700

    add my product

--- src/view/transaction/Create.vue
+++ src/view/transaction/Create.vue
@@ -8,27 +8,27 @@
                         Request Product
                     </div>
                     <div class="card-body">
-                        <form @submit.prevent="store()">
+                        <form @submit.prevent="onUpload()">
                             <div class="mb-3">
                                 <label for="" class="form-label">Product Name</label>
-                                <input type="text" class="form-control" v-model="transaction.title">
-                                <div v-if="validation.title" class="text-danger">
-                                    {{validation.title}}
+                                <input type="text" class="form-control" v-model="transaction.product_name">
+                                <div v-if="validation.product_name" class="text-danger">
+                                    {{validation.product_name}}
                                 </div>
                             </div>
                                <div class="mb-3">
                                 <label for="" class="form-label">Starting Price</label>
-                                <input type="number" class="form-control" v-model="transaction.amount">
-                                <div v-if="validation.amount" class="text-danger">
-                                    {{validation.amount}}
+                                <input type="number" class="form-control" v-model="transaction.starting_price">
+                                <div v-if="validation.starting_price" class="text-danger">
+                                    {{validation.starting_price}}
                                 </div>
                             </div>
                                <div class="mb-3">
                                 <label for="" class="form-label">End Date</label>
                                 
-                                <input type="date" class="form-control" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.time">
-                                <div v-if="validation.time" class="text-danger">
-                                    {{validation.time}}
+                                <input type="date" class="form-control" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.end_date">
+                                <div v-if="validation.end_date" class="text-danger">
+                                    {{validation.end_date}}
                                 </div>
                             </div>
                              <div class="mb-3">
@@ -43,10 +43,7 @@
                             </div>
                               <div class="mb-3">
                                 <label for="" class="form-label">Product Images</label>
-                                <input type="file" name="imagesArray" class="form-control" multiple @change="onChange" >
-                                <div v-if="validation.time" class="text-danger">
-                                    {{validation.time}}
-                                </div>
+                                <input type="file" name="imagesArray[]" class="form-control" multiple @change="onChange"  >
                             </div>
                             <button class="btn btn-outline-primary">Submit</button>
                         </form>
@@ -62,16 +59,16 @@
 import {reactive, ref} from 'vue'
 import {useRouter} from 'vue-router'
 import axios from 'axios'
-import * as fb from '../../utils/firebase.js'
+import * as auth from '../../utils/firebase-auth.js'
 
 export default {
     setup(){
         //data binding
         const transaction = reactive({
-            title: '',
-            amount: '',
-            time: '',
-            type: '',
+            product_name: '',
+            starting_price: '',
+            end_date: '',
+            type: ''
         });
 
         const validation = ref([]);
@@ -79,27 +76,7 @@ export default {
         const router = useRouter();
 
         function store(){
-            // axios.post('https://crudcrud.com/api/4682bf7fdfa14d94a4eb20c7cdd18aef/transaction'
-            // , transaction
-            // )
-            // .then((result) => {
-            //     router.push({
-            //         name: 'transaction.index'
-            //     });
-            // }).catch((err) => {
-            //     validation.value = err.response.data
-            // });
-
-
-            fb.set(fb.ref(fb.db, 'transactions/0'), transaction)
-           .then((result) => {
-                router.push({
-                    name: 'transaction.index' 
-
-                });
-            }).catch((err) => {
-                validation.value = err.response.data
-            });
+          
         }
 
         return {
@@ -117,30 +94,39 @@ export default {
      methods: {
         onChange (event) {
           this.imagesArray = event.target.files
-
-            let baseImageArr = [];
-
-            
-            for (const i of Object.keys(this.imagesArray)) {
-                  var reader = new FileReader();
-                reader.onloadend = function() {
-                    console.log('RESULT', reader.result)
-                }
-                reader.readAsDataURL(this.imagesArray[i]);
-                baseImageArr.push(this.imagesArray[i])
-            }
-          console.log(baseImageArr[0]);
         },
-        onUpload() {
+        async onUpload() {
+           let user = await auth.getUserSession();
+        console.log(user.uid)
+
           const formData = new FormData();
-          for (const i of Object.keys(this.imagesArray)) {
-            formData.append('imagesArray', this.imagesArray[i])
+
+          if (user != null){
+            for (const i of Object.keys(this.imagesArray)) {
+                formData.append('imagesArray[]', this.imagesArray[i])
+            }
+
+            formData.append('user_id',user.uid)
+            formData.append('is_active',0)
+            formData.append('name',this.transaction.product_name)
+            formData.append('starting_price',this.transaction.starting_price)
+            formData.append('end_date',this.transaction.end_date)
+            formData.append('type',this.transaction.type)
+
+        
+            axios.post('http://localhost/auction-backend/public/request-product', formData, {
+                headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+            }).then((res) => {
+                if(res.data.status == 200){
+                    console.log(res)
+                    this.router.push('/')
+                }
+            })
           }
-          console.log(formData);
-        //   axios.post('http://localhost:8888/api/multi-images-upload', formData, {
-        //   }).then((res) => {
-        //     console.log(res)
-        //   })
+
+          
         }  
     }  
 } 
---
+++ src/vue.config.js
@@ -0,0 +1,5 @@
+module.exports={
+    devServer: {
+        proxy: 'http://192.168.0.13/auction-backend/public'
+    }
+}
